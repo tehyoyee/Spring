f873b87325d10abe46060ca906ea71686730277d
tehyoyee <89984263+tehyoyee@users.noreply.github.com>
2023-04-26 19:42:21 +0900

    add repository/JdbcTemplateMemberRepository.java service/MemberServiceIntegrationTest.java, update SpringConfig.java

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,8 +1,7 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -26,7 +25,8 @@ public class SpringConfig {
 	@Bean
 	public MemberRepository memberRepository() {
 //		return new MemoryMemberRepository();
-		return new JdbcMemberRepository(dataSource);
+//		return new JdbcMemberRepository(dataSource);
+		return new JdbcTemplateMemberRepository(dataSource);
 	}
 }
 
