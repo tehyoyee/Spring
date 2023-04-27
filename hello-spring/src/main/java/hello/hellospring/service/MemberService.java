165f4e4d6b5bfa29ccafaedef57b83241b1104e9
tehyoyee <89984263+tehyoyee@users.noreply.github.com>
2023-04-27 14:01:40 +0900

    add aop/TimeTraceAop

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -24,20 +24,18 @@ public class MemberService {
 	 * 회원가입
 	 */
 	public Long join(Member member) {
-		// 같은 이름이 있는 중복 회원X
-//		Optional<Member> result = memberRepository.findByName(member.getName());
-//		result.ifPresent(m -> {
-//			throw new IllegalStateException("이미 존재하는 회원입니다.");
-//		});
-//		Optional 안이쁘다. 반환하는게 좋지않다. 그래서 아래와 같이도 한다.
-//		memberRepository.findByName(member.getName())
-//				.ifPresent(m -> {
-//				throw new IllegalStateException("이미 존재하는 회원입니다.");
-//		});
-		validateDuplicateMember(member);    // 중복 회원 검증
-		memberRepository.save(member);
 
-		return member.getId();
+		long start = System.currentTimeMillis();
+
+		try {
+			validateDuplicateMember(member);    // 중복 회원 검증
+			memberRepository.save(member);
+			return member.getId();
+		} finally {
+			long finish = System.currentTimeMillis();
+			long timeMs = finish - start;
+			System.out.println("join " + timeMs + "ms");
+		}
 	}
 
 	private void validateDuplicateMember(Member member) {
