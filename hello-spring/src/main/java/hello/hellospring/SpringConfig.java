ac60989a9a489a0a35923a7b3d31daac2e63f33d
tehyoyee <89984263+tehyoyee@users.noreply.github.com>
2023-04-27 11:11:47 +0900

    add JpaMemberRepository

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,21 +1,23 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
+import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-	private final DataSource dataSource;
+	private EntityManager em;
 
-	public SpringConfig(DataSource dataSource) {
-		this.dataSource = dataSource;
+	@Autowired
+	public SpringConfig(EntityManager em) {
+		this.em = em;
 	}
+
 
 	@Bean
 	public MemberService memberService() {
@@ -26,7 +28,8 @@ public class SpringConfig {
 	public MemberRepository memberRepository() {
 //		return new MemoryMemberRepository();
 //		return new JdbcMemberRepository(dataSource);
-		return new JdbcTemplateMemberRepository(dataSource);
+//		return new JdbcTemplateMemberRepository(dataSource);
+		return new JpaMemberRepository(em);
 	}
 }
 
